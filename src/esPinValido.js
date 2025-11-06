75242deea8de30478a472fe082546119e02b16c9
Mykyta Vavulin <nik.vavulin@gmail.com>
2025-11-06 18:59:44 +0100

    Funcion esPinValido refactorizada

--- src/esPinValido.js
+++ src/esPinValido.js
@@ -1,43 +1,26 @@
-function esPinValido(pin) {
+export function esPinValido(pin) {
 
-    if (pin === null || pin === undefined) {
-        return false;
-    }
-
-
-    if (pin === '') {
-        return false;
-    }
-
-    if (pin.length !== 4 && pin.length !== 6) {
-        return false;
-    }
+  if (!pin) {
+    return false;
+  }
 
 
-    for (let i = 0; i < pin.length; i++) {
-        const caracter = pin[i];
-        if (caracter < '0' || caracter > '9') {
-            return false;
-        }
-    }
+  if (pin.length !== 4 && pin.length !== 6) {
+    return false;
+  }
 
 
-    let primerNumero = pin[0];
-    let todosIguales = true;
-    for (let i = 1; i < pin.length; i++) {
-        if (pin[i] !== primerNumero) {
-            todosIguales = false;
-            break;
-        }
-    }
+  if (!/^\d+$/.test(pin)) {
+    return false;
+  }
 
-    if (todosIguales) {
-        return false;
-    }
-    return true;
-}
 
-export
-{
-    esPinValido
+  const primerNumero = pin[0];
+  const todosIguales = pin.split('').every(digito => digito === primerNumero);
+
+  if (todosIguales) {
+    return false;
+  }
+
+  return true;
 }
