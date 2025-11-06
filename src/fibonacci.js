18d5e09359aa8dd8e5a182befbb2a45c035f96cf
RBC <raqblacor2@alu.edu.gva.es>
2025-11-06 18:20:07 +0100

    Archivo fibonacci.js refactorizado

---
+++ src/fibonacci.js
@@ -0,0 +1,22 @@
+function fibonacci(n) {
+  if (!Number.isInteger(n) || n < 0) {
+    throw new RangeError("El argumento debe ser un número entero ≥ 0");
+  }
+
+  if (n <= 1) return n;
+
+  let [prev, curr] = [0, 1];
+
+  for (let i = 2; i <= n; i++) {
+    [prev, curr] = [curr, prev + curr];
+  }
+
+  return curr;
+}
+
+export{
+    fibonacci
+}
+
+
+
